1fac918e66a7561a7def10f4f4f76c7e493cf293
Bailey Popp <bpopp2016@yahoo.com>
2018-09-20 13:38:59 -0500

    changed tau_dcmotor

--- tau_dcmotor.m
+++ tau_dcmotor.m
@@ -1,4 +1,19 @@
 %tau_dcmotor
 
 function tau = tau_dcmotor(omega,motor)
+    if nargin ~= 2 
+        error('There is not 2 inputs.');
+    elseif ~isvector(omega)
+        error('Omega is not a vector.');
+    elseif ~isstruct(motor)
+        error('Motor is not a struct.');
+    else
+        if omega > 0 && omega < omegaNL
+            tau = tau;
+        elseif omega <0
+            tau = 0;
+        else omega > omegaNL
+            tau = taus;
+        end
+    end
 end
