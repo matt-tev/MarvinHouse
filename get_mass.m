317a82bbd7ca5f7503806e066417c782ab527b1a
Matt Teweleit <matttev@tamu.edu>
2018-09-20 13:33:44 -0500

    Updated get_mass

--- get_mass.m
+++ get_mass.m
@@ -1,3 +1,15 @@
 %get_mass
-%lots of mass
-max = 8;
+function m = get_mass(rover)
+disp(nargin)
+    if nargin ~= 1
+        error('There may only be one input.');
+    else if ~isstruct(rover)     
+        error('The input rover must be a struct.');
+    else
+        mass = chassis.mass + power_subsys.mass + science_payload.mass ...
+            + 6*(motor.mass + speed_reducer.mass + wheel.mass);
+    end
+        
+    m = mass;
+    
+end
