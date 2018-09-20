d5fecb7cf8d0ec3678b1e182abb858a4ff1eb493
Bailey Popp <bpopp2016@yahoo.com>
2018-09-20 13:48:50 -0500

    changed get_gear_ratio

--- get_gear_ratio.m
+++ get_gear_ratio.m
@@ -1,1 +1,10 @@
 %get_gear_ratio
+
+function Ng = get_gear_ratio(speed_reducer)
+    if nargin ~= 1
+        error('There is not 1 input.')
+    elseif ~isstruct(speed_reducer)
+        error('Speed Reducer is not a struct.')
+    else
+    end
+end
