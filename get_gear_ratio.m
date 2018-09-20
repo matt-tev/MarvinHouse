8a003d798aca4de4ced969776ca3a818e086fd64
Bailey Popp <bpopp2016@yahoo.com>
2018-09-20 14:01:26 -0500

    updates get_gear_ratio

--- get_gear_ratio.m
+++ get_gear_ratio.m
@@ -6,5 +6,9 @@ function Ng = get_gear_ratio(speed_reducer)
     elseif ~isstruct(speed_reducer)
         error('Speed Reducer is not a struct.')
     else
+        if s1 == speed_reducer.type && s2 == ('reverted')'
+            Ng = strcmp(s1,s2);
+        else error('The type field is not reverted.')
+        end
     end
 end
