c02c18607eca328285c7ffce68b1b881dd337505
Adam Maher <a1324@users.noreply.github.com>
2018-09-25 09:56:37 -0500

    Graphs_sr

---
+++ graphs_sr.m
@@ -0,0 +1,18 @@
+Ng = get_gear_ratio(speed_reducer)
+Tout = get_
+
+subplot(3,1,1)
+%speed_speed_reducer vs torque_speed_reducer
+plot(speed_reducer.speed
+xlabel('Torque (Nm)')
+ylabel('Speed (rad/s)')
+
+subplot(3,1,2)
+%power_speed_reducer vs torque_speed_reducer
+xlabel('Torque (Nm)')
+ylabel('Power (W)')
+
+subplot(3,1,3)
+%power_speed_reducer vs speed_speed_reducer
+xlabel('Speed (rad/s)')
+ylabel('Power (W)')
