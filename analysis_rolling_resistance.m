381bb7e6ce2be7b8e265964369036ebd4ac0a5c8
Bailey Popp <bpopp2016@yahoo.com>
2018-09-27 10:35:47 -0500

    update 2

--- analysis_rolling_resistance.m
+++ analysis_rolling_resistance.m
@@ -2,7 +2,7 @@
 
 Crr_list = linspace(0.01,0.4,25);
 omega = motor.speed_noload*(1 - ((tau_dcmotor(omega,motor)) - (motor.torque_noload))/((motor.torque_stall) - (motor.torque_noload)));
-Vrover = wheel.radius*(omega/get_gear_ratio(rover.speed_reducer));
+Vrover = wheel.radius*(omega/get_gear_ratio(rover.wheel_assembly.speed_reducer));
 fun = Vrover;
 x0 = motor.speed_noload;
 v_max = fzero(fun,x0);
