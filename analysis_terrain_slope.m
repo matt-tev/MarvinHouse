8b15ccc37e812eb3b7216b3ebb74e1eda53b1e4e
Bailey Popp <bpopp2016@yahoo.com>
2018-09-27 13:40:42 -0500

    think i got it

--- analysis_terrain_slope.m
+++ analysis_terrain_slope.m
@@ -6,9 +6,9 @@ Crr = 0.2;
 slope_list_deg = linspace(-10,35,25);
 x0 = motor.speed_noload;
 for i=1:length(slope_list_deg)
-    v_max(i) = radius*fzero(@(omega)F_net(omega,slope_list_deg,rover,planet,Crr),x0)/NG;
+    v_max(i) = radius*fzero(@(omega)F_net(omega,slope_list_deg(i),rover,planet,Crr),x0)/NG;
 end
 
-plot(v_max(i),slope_list_deg);
+plot(v_max,slope_list_deg);
 xlabel('Velocity Max (m/s)');
 ylabel('Slope (deg)');
