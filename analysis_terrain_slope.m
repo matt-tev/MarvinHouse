371db8edcb7a1a90a35aaf4aa70855ad1c6ce35c
Bailey Popp <bpopp2016@yahoo.com>
2018-09-27 11:21:46 -0500

    update

--- analysis_terrain_slope.m
+++ analysis_terrain_slope.m
@@ -2,10 +2,9 @@
 
 Crr = 0.2;
 slope_list_deg = linspace(-10,35,25);
-fun = Vrover(rover,omega);
 x0 = motor.speed_noload;
-v_max = fzero(@Vrover(rover,omega),x0);
+v_max = fzero(@(omega)Vrover(rover,omega),x0);
 
 plot(v_max,slope_list_deg);
-xlabel('v_max');
-ylabel('slope_list_deg');
+xlabel('v_max (m/s)');
+ylabel('slope_list_deg (deg)');
