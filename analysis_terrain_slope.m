d834672365156c3bf0ae55212b855c848d6d9dcd
Bailey Popp <bpopp2016@yahoo.com>
2018-09-27 13:24:24 -0500

    update

--- analysis_terrain_slope.m
+++ analysis_terrain_slope.m
@@ -1,12 +1,14 @@
 % script called analysis_terrain_slope
 
-Ng = get_gear_ratio(speed_reducer);
+NG = get_gear_ratio(speed_reducer);
+radius = wheel.radius;
 Crr = 0.2;
 slope_list_deg = linspace(-10,35,25);
 x0 = motor.speed_noload;
 for i=1:length(slope_list_deg)
-    v_max(i) = fzero(@(omega)F_net(omega,terrain_angle,rover,planet,Crr),x0);
+    v_max(i) = radius*fzero(@(omega)F_net(omega,slope_list_deg,rover,planet,Crr),x0)/NG;
 end
+
 plot(v_max(i),slope_list_deg);
 xlabel('Velocity Max (m/s)');
 ylabel('Slope (deg)');
