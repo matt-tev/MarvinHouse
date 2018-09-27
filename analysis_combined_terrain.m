298c05b3cf6ca63ca9cda221ab443faff1697b5b
Bailey Popp <bpopp2016@yahoo.com>
2018-09-27 09:41:42 -0500

    update analysis_combined_terrain

--- analysis_combined_terrain.m
+++ analysis_combined_terrain.m
@@ -4,14 +4,13 @@ Crr_list = linspace(0.01,0.4,25);
 slope_list_deg = linspace(-10,35,25);
 [CRR,SLOPE] = meshgrid(Crr_list, slope_list_deg);
 VMAX = zeros(size(CRR));
-omega = motor.speed_noload*(1 - ((tau_dcmotor(omega,motor)) - (motor.torque_noload))/((motor.torque_stall) - (motor.torque_noload)));
 
 N = size(CRR,1);
 for i=1:N
     for j=1:N
-        Crr_sample = CRR(i,j);
-        slope_sample = SLOPES(i,j);
-        VMAX(i,j) = wheel.radius*(omega/get_gear_ratio(rover.speed_reducer)); 
+        Crr = Crr_list(i,j);
+        slope = slope_list_deg(i,j);
+        VMAX(i,j) = wheel.radius*(omega/get_gear_ratio(rover.wheel_assembly.speed_reducer)); 
             
     end
 end
