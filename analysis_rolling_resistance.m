b402f73feabc8ce681077d276b91f596e3ec63c4
Bailey Popp <bpopp2016@yahoo.com>
2018-09-27 10:36:51 -0500

    don't need omega

--- analysis_rolling_resistance.m
+++ analysis_rolling_resistance.m
@@ -1,7 +1,6 @@
 % analysis_rolling_resistance
 
 Crr_list = linspace(0.01,0.4,25);
-omega = motor.speed_noload*(1 - ((tau_dcmotor(omega,motor)) - (motor.torque_noload))/((motor.torque_stall) - (motor.torque_noload)));
 Vrover = wheel.radius*(omega/get_gear_ratio(rover.wheel_assembly.speed_reducer));
 fun = Vrover;
 x0 = motor.speed_noload;
--- analysis_terrain_slope.m
+++ analysis_terrain_slope.m
@@ -2,8 +2,6 @@
 
 Crr = 0.2;
 slope_list_deg = linspace(-10,35,25);
-% omega comes from equation on page 6
-omega = motor.speed_noload*(1 - ((tau_dcmotor(omega,motor)) - (motor.torque_noload))/((motor.torque_stall) - (motor.torque_noload)));
 Vrover = wheel.radius*(omega/get_gear_ratio(rover.wheel_assembly.speed_reducer));
 fun = Vrover;
 x0 = motor.speed_noload;
