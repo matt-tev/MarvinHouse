bc5811397d9d99b1fb8aae8efc39865ef008082d
Bailey Popp <bpopp2016@yahoo.com>
2018-09-27 13:55:34 -0500

    got it

--- analysis_combined_terrain.m
+++ analysis_combined_terrain.m
@@ -1,6 +1,8 @@
 % analysis_combined_terrain takes the rolling resistance coefficients and 
 % slope angles to find the maximum velocity
 
+NG = get_gear_ratio(speed_reducer);
+radius = wheel.radius;
 Crr_list = linspace(0.01,0.4,25);
 slope_list_deg = linspace(-10,35,25);
 [CRR,SLOPE] = meshgrid(Crr_list, slope_list_deg);
@@ -11,7 +13,7 @@ for i=1:N
     for j=1:N
         Crr_sample = CRR(i,j);
         slope_sample = SLOPE(i,j);
-        VMAX(i,j) = wheel.radius*(omega/get_gear_ratio(rover.wheel_assembly.speed_reducer));     
+            VMAX(i,j) = radius*fzero(@(omega)F_net(omega,slope_sample,rover,planet,Crr_sample),x0)/NG;
     end
 end
    
