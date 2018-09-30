c5068c0f68140540d93f0e53fa05b5ecfb2f2471
Matt Teweleit <matttev@tamu.edu>
2018-09-29 19:29:59 -0500

    Dr. Steven Brule

--- analysis_rolling_resistance.m
+++ analysis_rolling_resistance.m
@@ -4,6 +4,8 @@ NG = get_gear_ratio(speed_reducer);
 radius = wheel.radius;
 Crr_list = linspace(0.01,0.4,25);
 x0 = motor.speed_noload;
+v_max = zeros(1,length(Crr_list));
+
 for i = 1:length(Crr_list)
     v_max(i) = radius*fzero(@(omega)F_net(omega,terrain_angle,rover,planet,Crr_list(i)),x0)/NG;
 end
--- analysis_terrain_slope.m
+++ analysis_terrain_slope.m
@@ -4,6 +4,8 @@ NG = get_gear_ratio(speed_reducer);
 radius = wheel.radius;
 Crr = 0.2;
 slope_list_deg = linspace(-10,35,25);
+v_max = zeros(1,length(slope_list_deg));
+
 x0 = motor.speed_noload;
 for i=1:length(slope_list_deg)
     v_max(i) = radius*fzero(@(omega)F_net(omega,slope_list_deg(i),rover,planet,Crr),x0)/NG;
