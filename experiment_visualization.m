46901a4134710499097bc89be2c8f2afaa44f9bf
Adam Maher <a1324@users.noreply.github.com>
2018-10-29 14:04:56 -0500

    push

--- experiment_visualization.m
+++ experiment_visualization.m
@@ -5,6 +5,12 @@ load experiment1
 x = linspace(min(experiment.alpha_dist),max(experiment.alpha_dist));
 terrain_angle = interp1(experiment.alpha_dist,experiment.alpha_deg,x,'spline');
 figure
-plot(x,terrain_angle)
+plot(x,terrain_angle,'k','LineWidth',1)
 xlabel('Distance (m)')
 ylabel('Terrain Angle (deg)')
+title('Terrain Angle vs. Distance')
+hold on 
+xfile = experiment.alpha_dist;
+yfile = experiment.alpha_deg;
+plot(xfile,yfile,'r*')
+hold off
