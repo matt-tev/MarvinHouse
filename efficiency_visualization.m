3698c0e04e350d0ccdfd6aa7e5958c3be2215eb4
Matt Teweleit <matttev@tamu.edu>
2018-10-31 15:13:39 -0500

    push

--- efficiency_visualization.m
+++ efficiency_visualization.m
@@ -1,7 +1,7 @@
 %efficiency_visualization
 tau_data = motor.effcy_tau;
 eff_data = motor.effcy*100;
-tau_motor = linspace(0,170);
+tau_motor = linspace(0,170,100);
 eff = interp1(tau_data,eff_data,tau_motor,'spline');
 plot(tau_motor,eff,'k','LineWidth',1)
 xlabel('Torque (N-m)')
