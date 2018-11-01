29b8b2027697a6f88e0b05e68a32cc88c17f3269
Adam Maher <a1324@users.noreply.github.com>
2018-11-01 10:47:23 -0500

    push

--- experiment_visualization.m
+++ experiment_visualization.m
@@ -1,16 +1,20 @@
 %experient_visualization
 % load the given data from the file
 load experiment1
-% calculate terrain angles
+% create a distance variable from minimum to maximum
 x = linspace(min(experiment.alpha_dist),max(experiment.alpha_dist));
+% calculate terrain angles
 terrain_angle = interp1(experiment.alpha_dist,experiment.alpha_deg,x,'spline');
 figure
+% plot the calculated terrain angle vs distance
 plot(x,terrain_angle,'k','LineWidth',1)
 xlabel('Distance (m)')
 ylabel('Terrain Angle (deg)')
 title('Terrain Angle vs. Distance')
 hold on 
+% create arrays of the data from the given struct
 xfile = experiment.alpha_dist;
 yfile = experiment.alpha_deg;
+% plot these arrays on the same graph as the previous data 
 plot(xfile,yfile,'r*')
 hold off
